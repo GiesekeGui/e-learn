33d20c14b60764ed1a3df8402b4671a07577b88e
Gieseke <jugieseke@gmail.com>
2023-07-07 12:35:46 -0300

    Revert "Atualização 07.07.2023"
    This reverts commit 36a128e8e5ad4c329333b43ba1f3384acaef95ca.

--- Exercícios2/Exercício1.py
+++
@@ -1,7 +0,0 @@
-velocidade = float(input('insira a velocidade em Km/h:'))
-
-if velocidade > 80:
-  print('Seu veículo foi multado.')
-  print(f'Devido sua velocidade excessiva você vai ter que pagar R${(velocidade - 80) * 7} de multa.')
-else:
-  print('Tudo de boa com sua velocidade.')
