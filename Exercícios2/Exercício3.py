47f767707501c4de39e16c9c4b4c9789ae0d716c
GiesekeGui <giesekeguilherme@gmail.com>
2023-07-13 11:48:43 -0300

    Exercício 3

---
+++ Exercícios2/Exercício3.py
@@ -0,0 +1,9 @@
+preço = 0
+
+distância = float(input('insira a distância da viagem:'))
+if distância <= 200:
+ preço = distância * 0.45
+else:
+ preço = distância * 0.50
+
+print(f'Pela distância (distância)Km, tem que pagar:(preço)R$.')
